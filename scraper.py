d18f43c058a9921d424bd0e79ccb8cd800cefb2d
reichaves <reichaves@users.noreply.github.com>
2017-06-04 17:46:55 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,12 +17,12 @@ print html
 # We use lxml, which is a Python library especially for parsing html.
 # -----------------------------------------------------------------------------
 
-#import lxml.html
-#root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-#tds = root.cssselect('td') # get all the <td> tags
-#for td in tds:
-#    print lxml.html.tostring(td) # the full HTML tag
-#    print td.text                # just the text inside the HTML tag
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
 
 # -----------------------------------------------------------------------------
 # 2. Save the data in the ScraperWiki datastore.
@@ -31,9 +31,9 @@ print html
 # Check the 'Data' tab - here you'll see the data saved in the ScraperWiki store. 
 # -----------------------------------------------------------------------------
 
-#for td in tds:
-#     record = { "td" : td.text } # column name and value
-#     scraperwiki.sqlite.save(["td"], record) # save the records one by one
+for td in tds:
+     record = { "td" : td.text } # column name and value
+     scraperwiki.sqlite.save(["td"], record) # save the records one by one
     
 # -----------------------------------------------------------------------------
 # Go back to the Tutorials page and continue to Tutorial 3 to learn about 
